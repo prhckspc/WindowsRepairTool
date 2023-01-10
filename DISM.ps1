31575c8f18c91260b470497b9993fd957a493681
prhckspc <localhost@localdomain.local>
2023-01-10 17:00:48 +0100

    Version 1.0 done.

--- DISM.ps1
+++
@@ -1,3 +0,0 @@
-﻿#Import-Module DISM | DISM /Online /Cleanup-Image /RestoreHealth | Select-String -Pattern '\d\d\.\d\%|\d\.\d\%|\d\d\d\.\d\%'| Select -Expand Matches | Select -Expand Value
-Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
-Repair-WindowsImage -RestoreHealth -Online
